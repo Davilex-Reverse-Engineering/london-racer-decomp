a11b9f83cc00efa4fd75ef471fa1784e91b346f3
Wouter Wijsman <wwijsman@live.nl>
2026-08-02 23:57:46 +0200

    Add file for library functions in the code

---
+++ src/library_functions.hpp
@@ -0,0 +1,20 @@
+#ifndef LIBRARY_FUNCTIONS
+#define LIBRARY_FUNCTIONS
+// This file makes reccmp understand that some functions are specific library functions
+
+// LIBRARY: SPEL 0x00457fe0
+// sprintf
+
+// LIBRARY: SPEL 0x00457eee
+// fopen
+
+// LIBRARY: SPEL 0x004a087a
+// _snprintf
+
+// LIBRARY: SPEL 0x004a08ca
+// fprintf
+
+// LIBRARY: SPEL 0x00457d6e
+// fclose
+
+#endif /* LIBRARY_FUNCTIONS */
